2f3cd4aeff6a7b826e0582f9a799feeba907579a
PoetOrpheus <denisdevatkin6033@gmail.com>
2025-12-25 09:38:39 -0800

    Initial Commit

---
+++ KsyIOS/KsyIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KsyIOSApp.swift
+//  KsyIOS
+//
+//  Created by MAGNUM on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct KsyIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
