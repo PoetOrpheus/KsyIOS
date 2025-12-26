67f3fa07527306c503349448470ba649d467f3d4
denis <denisdevatkin6032@gmail.com>
2025-12-26 20:02:04 +0300

    123

--- KsyIOS/Views/Components/ProductGrid.swift
+++ KsyIOS/Views/Components/ProductGrid.swift
@@ -7,26 +7,6 @@
 
 import SwiftUI
 
-extension View {
-    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-        clipShape(RoundedCornerShape(radius: radius, corners: corners))
-    }
-}
-
-struct RoundedCornerShape: Shape {
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(
-            roundedRect: rect,
-            byRoundingCorners: corners,
-            cornerRadii: CGSize(width: radius, height: radius)
-        )
-        return Path(path.cgPath)
-    }
-}
-
 struct ProductGrid: View {
     let products: [Product]
     let onProductClick: (Product) -> Void
