c0407087b907f93a5893ad3a56d66ef21aa870b3
denis <denisdevatkin6032@gmail.com>
2025-12-27 14:14:54 +0300

    123

--- KsyIOS/Views/Components/Favorites/SettingsFavoriteRow.swift
+++ KsyIOS/Views/Components/Favorites/SettingsFavoriteRow.swift
@@ -14,83 +14,85 @@ struct SettingsFavoriteRow: View {
     
     var body: some View {
         HStack(spacing: 0) {
-            // Кнопка "Фильтр"
-            Button(action: {
-                // TODO: Открыть фильтр
-            }) {
-                ZStack {
-                    Color.white
-                        .frame(width: FigmaDimens.fw(30), height: FigmaDimens.fh(30))
-                        .cornerRadius(10)
-                    
-                    Group {
-                        if let uiImage = UIImage(named: "filtr_setting") {
-                            Image(uiImage: uiImage)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: FigmaDimens.fw(18), height: FigmaDimens.fh(18))
-                        } else {
-                            Image(systemName: "slider.horizontal.3")
-                                .foregroundColor(.black)
-                                .font(.system(size: 18))
+            // Левая часть: кнопки фильтр и категории
+            Group {
+                // Кнопка "Фильтр"
+                Button(action: {
+                    // TODO: Открыть фильтр
+                }) {
+                    ZStack {
+                        Color.white
+                            .frame(width: FigmaDimens.fw(30), height: FigmaDimens.fh(30))
+                            .cornerRadius(10)
+                        
+                        Group {
+                            if let uiImage = UIImage(named: "filtr_setting") {
+                                Image(uiImage: uiImage)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: FigmaDimens.fw(18), height: FigmaDimens.fh(18))
+                            } else {
+                                Image(systemName: "slider.horizontal.3")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 18))
+                            }
                         }
                     }
                 }
-            }
-            
-            Spacer().frame(width: FigmaDimens.fw(20))
-            
-            // Кнопка "Категории"
-            Button(action: {
-                // TODO: Открыть категории
-            }) {
-                ZStack {
-                    Color.white
-                        .frame(width: FigmaDimens.fw(30), height: FigmaDimens.fh(30))
-                        .cornerRadius(10)
-                    
-                    Group {
-                        if let uiImage = UIImage(named: "category_favorite_setting") {
-                            Image(uiImage: uiImage)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: FigmaDimens.fw(18), height: FigmaDimens.fh(18))
-                        } else {
-                            Image(systemName: "square.grid.2x2")
-                                .foregroundColor(.black)
-                                .font(.system(size: 18))
+                
+                Spacer().frame(width: FigmaDimens.fw(20))
+                
+                // Кнопка "Категории"
+                Button(action: {
+                    // TODO: Открыть категории
+                }) {
+                    ZStack {
+                        Color.white
+                            .frame(width: FigmaDimens.fw(30), height: FigmaDimens.fh(30))
+                            .cornerRadius(10)
+                        
+                        Group {
+                            if let uiImage = UIImage(named: "category_favorite_setting") {
+                                Image(uiImage: uiImage)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: FigmaDimens.fw(18), height: FigmaDimens.fh(18))
+                            } else {
+                                Image(systemName: "square.grid.2x2")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 18))
+                            }
                         }
                     }
                 }
+                
+                Spacer().frame(width: FigmaDimens.fw(40))
             }
             
-            Spacer().frame(width: FigmaDimens.fw(40))
-            
-            // "В наличии"
-            Text("В наличии")
-                .font(.system(size: 12, weight: .medium))
-                .foregroundColor(Color(hex: "353535") ?? .black)
-            
-            Spacer().frame(width: FigmaDimens.fw(10))
-            
-            // Переключатель "В наличии"
-            FavoriteSwitch(isOn: $isInStockEnabled)
-            
-            Spacer().frame(width: FigmaDimens.fw(20))
-            
-            // "По скидке"
-            Text("По скидке")
-                .font(.system(size: 12, weight: .medium))
-                .foregroundColor(Color(hex: "353535") ?? .black)
-            
-            Spacer().frame(width: FigmaDimens.fw(10))
-            
-            // Переключатель "По скидке"
-            FavoriteSwitch(isOn: $isSaleEnabled)
-            Spacer() // Добавляем Spacer в конце для правильного выравнивания
+            // Правая часть: переключатели "В наличии" и "По скидке"
+            Group {
+                Text("В наличии")
+                    .font(.system(size: 12, weight: .medium))
+                    .foregroundColor(Color(hex: "353535") ?? .black)
+                
+                Spacer().frame(width: FigmaDimens.fw(10))
+                
+                FavoriteSwitch(isOn: $isInStockEnabled)
+                
+                Spacer().frame(width: FigmaDimens.fw(20))
+                
+                Text("По скидке")
+                    .font(.system(size: 12, weight: .medium))
+                    .foregroundColor(Color(hex: "353535") ?? .black)
+                
+                Spacer().frame(width: FigmaDimens.fw(10))
+                
+                FavoriteSwitch(isOn: $isSaleEnabled)
+                
+                Spacer() // Финальный Spacer для выравнивания
+            }
         }
         .frame(height: FigmaDimens.fh(32))
         .padding(.horizontal, FigmaDimens.fw(15))
     }
 }
-
